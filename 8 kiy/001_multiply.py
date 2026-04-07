f85fd743d1100a342a498986435c76d006c9c202
abdur-rahman-c1 <abdur.rahman.contact1@gmail.com>
2026-04-07 23:00:36 +0600

    multiply and make negetive practice is done

---
+++ 8 kiy/001_multiply.py
@@ -0,0 +1,22 @@
+"""
+Title: Multiply
+Description: This code does not execute properly. Try to figure out why.
+"""
+
+
+#Problem:
+def multiply(a, b):
+     a * b
+
+
+# solution:
+def multiply(a, b):
+    return a * b
+
+
+"""
+The bug: The original code multiplies the number but never gives it back.
+
+The result: To get a result back, you Must use 'return' keywword
+
+"""
---
+++ 8 kiy/002_make_neegative.py
@@ -0,0 +1,30 @@
+"""
+Url: https://www.codewars.com/kata/55685cd7ad70877c23000102/train/python
+
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+Examples
+
+make_negative(1);  # return -1
+make_negative(-5); # return -5
+make_negative(0);  # return 0
+
+Notes
+
+    The number can be negative already, in which case no change is required.
+    Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+"""
+
+
+
+def make_negetive(number):
+    if number > 0: # if the number is smaller than 0
+        negetive = -number # make it negetive using unary minus
+        return negetive
+    elif number <= 0: # if it's already 0 or negetive
+        return number # return as it is
+    
+
+print(make_negetive(5))
+
+
