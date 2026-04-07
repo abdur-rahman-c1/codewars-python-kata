ba8e89f4c42496d47e5154c6cbce31d7743d6c04
abdur-rahman-c1 <abdur.rahman.contact1@gmail.com>
2026-04-08 00:35:08 +0600

    reversing simple strings with mutliple methods

---
+++ 8 kiy/004_reversed_strings.py
@@ -0,0 +1,23 @@
+"""
+Description:
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+
+"""
+
+# solution 1:
+def solution(string: str):
+    return string[::-1]  # uses string slice while keeping everything at default including start and stop. only the step is told to be in a reverse order. 
+
+# solution 2:
+def solution(string: str):
+    return ''.join(reversed(string))
+
+# reversed returns an iterator version of the given string in reverse order
+# it essentially is like vending machine where string are sorted and spits out one string after another. you can't see it, it's hidden inside of ram storage
+# ''.join catches all spitted out strings and joins them together
+
