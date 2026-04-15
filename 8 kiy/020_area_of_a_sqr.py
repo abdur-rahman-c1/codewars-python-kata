258661dd353b31d0d2271a67fb323ee0af21b416
abdur-rahman-c1 <abdur.rahman.contact1@gmail.com>
2026-04-16 05:44:12 +0600

    finished 20th problem

---
+++ 8 kiy/020_area_of_a_sqr.py
@@ -0,0 +1,12 @@
+"""
+Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+"8 kiy\square.png"
+
+Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+"""
+
+import math
+
+def square_area(a):
+    return (2 * a / math.pi) ** 2
