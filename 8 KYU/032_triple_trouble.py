be01fe264dfeeef54f82aa442a1359824aa86cd1
abdur-rahman-c1 <abdur.rahman.contact1@gmail.com>
2026-04-20 07:52:54 +0600

    Discovered about zip which allows you to perform loop in parallal

---
+++ 8 KYU/032_triple_trouble.py
@@ -0,0 +1,20 @@
+"""
+Triple Trouble
+
+Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+Note: You can expect all of the inputs to be the same length.
+
+"""
+
+
+def triple_trouble(one, two, three):
+    result = []
+    for i in range(len(one)):
+        result.extend([one[i], two[i], three[i]])
+    return "".join(result)
+
+def triple_trouble_v2(one, two, three):
+    return "".join(a + b + c for a, b, c in zip(one, two , three))
